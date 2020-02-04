4d2c046a9da5a15c20042405ce0ecc86330b040c
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-02-04 12:41:09 -0500

    Implements SimpleConsumer for #6

--- consumer/src/main/java/com/NSA/consumer/SimpleConsumer.java
+++ consumer/src/main/java/com/NSA/consumer/SimpleConsumer.java
@@ -1,13 +1,50 @@
 package com.NSA.consumer;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Properties;
+import java.time.Duration;
+import java.util.Arrays;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class SimpleConsumer {
+   public static void main(String[] args) throws Exception {
+      if(args.length == 0){
+         System.out.println("Enter topic name");
+         return;
+      }
+      //Kafka consumer configuration settings
+      String topicName = args[0].toString();
+      Properties props = new Properties();
+      
+      props.put("bootstrap.servers", "localhost:9092");
+      props.put("group.id", "test");
+      props.put("enable.auto.commit", "true");
+      props.put("auto.commit.interval.ms", "1000");
+      props.put("session.timeout.ms", "30000");
+      props.put("key.deserializer", 
+    		  StringDeserializer.class.getName());
+      props.put("value.deserializer", 
+    		  StringDeserializer.class.getName());
+      KafkaConsumer<String, String> consumer = new KafkaConsumer
+         <String, String>(props);
+      
+      //Kafka Consumer subscribes list of topics here.
+      consumer.subscribe(Arrays.asList(topicName));
+      
+      //print the topic name
+      System.out.println("Subscribed to topic " + topicName);
+      int i = 0;
+      
+      while (true) {
+    	Duration consumerTimeout = Duration.ofMillis(100);
+		ConsumerRecords<String, String> records = consumer.poll(consumerTimeout);
+         for (ConsumerRecord<String, String> record : records)
+         
+         // print the offset,key and value for the consumer records.
+         System.out.printf("offset = %d, key = %s, value = %s\n", 
+            record.offset(), record.key(), record.value());
+      }
+   }
 }
