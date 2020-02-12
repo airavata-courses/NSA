fc7c1a825002a96582074fc9340d49cc3b6851b5
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-02-12 00:05:04 -0500

    Added api-gateway (KafkaProducer)

---
+++ KafkaProducer/src/main/java/com/example/demo/ApiGatewayResource.java
@@ -0,0 +1,92 @@
+package com.example.demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.kafka.common.requests.ApiVersionsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("kafka")
+public class ApiGatewayResource {
+
+	@Autowired
+	KafkaTemplate<String,String> kafkaTemplate;
+	private static final String TOPIC_LOGIN_MESSAGE= "KafkaLoginMessage";
+	private static final String TOPIC_REGISTER_MESSAGE= "KafkaRegisterMessage";
+	private static final String TOPIC_DATARETRIVE_MESSAGE= "messagehandler-dataretreival";
+	
+	@Autowired
+	KafkaListenerRegisterFeeback registerAckService;
+	@Autowired
+	KafkaListenerLoginFeeback loginAckService;
+	@Autowired
+	KafkaListenerDataRetrieval dataAckService;
+	
+	@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+	@RequestMapping(value="/login", method = RequestMethod.POST, consumes = "application/json")
+	public String login(@RequestBody String message) throws InterruptedException {
+		//String message=user.getFirstName();
+		System.out.println("**************** OTIIII **********"+ message);
+		kafkaTemplate.send(TOPIC_LOGIN_MESSAGE,message);
+		
+		String ack= loginAckService.returnFeedback();
+		System.out.println("ack is"+ack);
+		System.out.println("condition is"+ ack.equalsIgnoreCase("checking"));
+	//	while(ack.equalsIgnoreCase("checking")) {
+		TimeUnit.SECONDS.sleep(10);
+		ack= loginAckService.returnFeedback();
+		//}
+		return ack;
+	}
+	
+	@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+	@RequestMapping(value="/register", method = RequestMethod.POST, consumes = "application/json")
+	public String register(@RequestBody String message) throws InterruptedException {
+		kafkaTemplate.send(TOPIC_REGISTER_MESSAGE,message);
+		
+		String ack= registerAckService.returnFeedback();
+		System.out.println("ack is"+ack);
+		System.out.println("condition is"+ ack.equalsIgnoreCase("failure"));
+	//	while(ack.equalsIgnoreCase("checking")) {
+		TimeUnit.SECONDS.sleep(10);
+		ack= registerAckService.returnFeedback();
+		
+		return ack;
+	}
+
+	
+	
+	@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+	@RequestMapping(value="/dataretrieval", method = RequestMethod.POST, consumes = "application/json")
+	public String dataRetrival(@RequestBody String message) throws InterruptedException {
+		kafkaTemplate.send(TOPIC_DATARETRIVE_MESSAGE,message);
+		//Just push the data to message broker for dataa
+		
+		String ack= dataAckService.returnFeedback();
+		System.out.println("ack is"+ack);
+		System.out.println("condition is"+ ack.equalsIgnoreCase("failure"));
+	//	while(ack.equalsIgnoreCase("checking")) {
+		TimeUnit.SECONDS.sleep(10);
+		ack= dataAckService.returnFeedback();
+		
+		return ack;
+	}
+	
+	
+	@RequestMapping("/")
+	public String showLoginForm() {
+
+		return "Login.jsp";
+	}
+}
