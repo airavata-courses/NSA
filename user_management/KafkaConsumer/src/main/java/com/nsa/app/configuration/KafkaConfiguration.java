6f6e0a0bc1c0339b8b28246443f79e6bd61b8f53
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-02-29 21:24:28 -0500

    Addresses #43 (api_gateway, user_management, mysql)

---
+++ user_management/KafkaConsumer/src/main/java/com/nsa/app/configuration/KafkaConfiguration.java
@@ -0,0 +1,69 @@
+package com.nsa.app.configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+@EnableKafka
+@Configuration
+public class KafkaConfiguration {
+
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory(){
+		Map<String,Object> config =new HashMap<>();
+		
+		config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+		config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
+		config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		
+		return new DefaultKafkaConsumerFactory<>(config);
+	}
+	
+	
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory kafkaListenerContainerFactory() {
+		
+		ConcurrentKafkaListenerContainerFactory<String,String> factory= new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		
+		return factory;
+		
+	}
+	
+	
+	// Usermanagement producer to return the response of login
+	@Bean 
+	public  ProducerFactory producerFactory(){
+		
+		Map<String,Object> config= new HashMap<>();
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"kafka:9092" );
+		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class );
+		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,JsonSerializer.class );
+				
+		return new DefaultKafkaProducerFactory<>(config);
+				
+	}
+	
+	
+	@Bean
+	public KafkaTemplate<String,String> kafkaTemplate(){
+		return new KafkaTemplate<String,String>(producerFactory());
+	}
+	
+	
+}
