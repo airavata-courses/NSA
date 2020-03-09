13d8d8f68d225a12239bbc4a88e1f456c3c4464a
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-03-09 00:25:43 -0400

    Init from dockerize

---
+++ user_management/KafkaConsumer/src/main/java/com/nsa/app/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.nsa.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.nsa.app.model.User;
+
+@Repository("userRepository")
+public interface UserRepository extends CrudRepository<User,Integer>{
+	User findByuserName (String userName);
+	 
+	User findByLastName (String username);
+	 
+	boolean existsById (int id);
+	 
+}
