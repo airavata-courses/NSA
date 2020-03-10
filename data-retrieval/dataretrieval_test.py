91d2dc23553e0c6557e6b9494ffd8898f11a1f52
nbafna <nikitabafna04@gmail.com>
2020-03-10 19:26:07 -0400

    Address #82

---
+++ data-retrieval/dataretrieval_test.py
@@ -0,0 +1,13 @@
+import unittest
+from dataretrieval import get_scans
+
+class Tests(unittest.TestCase):
+    def dr_test(self):
+        print("Testing Radar information")
+        expected = [<AwsNexradFile object - 2016/05/31/KTLX/KTLX20160531_000848_V06.gz>]
+        result = get_scans('2016','05','31','KTLX')
+        self.assertEqual(result,expected,msg = "Scan failed")
+
+if __name__ == '__main__':
+	print("Test component for Data Retrieval")
+	unittest.main()
