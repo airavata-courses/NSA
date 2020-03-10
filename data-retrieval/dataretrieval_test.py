82811e16eab573e8f6a2121cf1c095cdcdd2ba1f
nbafna <nikitabafna04@gmail.com>
2020-03-10 19:28:38 -0400

    Address #82

--- data-retrieval/dataretrieval_test.py
+++ data-retrieval/dataretrieval_test.py
@@ -4,7 +4,7 @@ from dataretrieval import get_scans
 class Tests(unittest.TestCase):
     def dr_test(self):
         print("Testing Radar information")
-        expected = [<AwsNexradFile object - 2016/05/31/KTLX/KTLX20160531_000848_V06.gz>]
+        expected = ["<AwsNexradFile object - 2016/05/31/KTLX/KTLX20160531_000848_V06.gz>"]
         result = get_scans('2016','05','31','KTLX')
         self.assertEqual(result,expected,msg = "Scan failed")
 
