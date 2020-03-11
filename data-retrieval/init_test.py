eb561c31aff183592866e8ab505a345c9eeb7160
nbafna <nikitabafna04@gmail.com>
2020-03-10 20:11:31 -0400

    Address #82

--- data-retrieval/init_test.py
+++ data-retrieval/init_test.py
@@ -1,6 +1,6 @@
 import nexradaws
 
-def get_scans(self,years,months,days,radars):
+def get_scans(years,months,days,radars):
     test_conn = nexradaws.NexradAwsInterface()
     scans = test_conn.get_avail_scans(years, months, days, radars);
     return str(scans[0])
