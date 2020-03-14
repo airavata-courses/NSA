da7af653fcee335625bb70089109b9082bcae244
nbafna <nikitabafna04@gmail.com>
2020-03-14 16:11:14 -0400

    Adding test cases

--- session-management/session-management.js
+++ session-management/session-management.js
@@ -6,6 +6,18 @@ var bodyParser = require('body-parser')
 var cors = require('cors')
 
 main = () => {
+
+
+  sess.get('/', function (req, res) {
+    if (req.session.page_views) {
+      req.session.page_views++;
+      res.send("You visited this page " + req.session.page_views + " times");
+    } else {
+      req.session.page_views = 1;
+      res.send("Welcome to this page for the first time!");
+    }
+  });
+  
   const consumer = require('./config/kafkaconfig').consumer;
   const calls = require('./sessionservice/sessioncontroller');
 
@@ -23,30 +35,24 @@ main = () => {
   consumer.on('message', function (message) {
 
     if (message.topic == 'login-sessionmgmt') {
+      message = JSON.parse(message.value)
       calls.createdata(message)
 
     } else if (message.topic == 'dataretrieval-sessionmgmt') {
+      message = JSON.parse(message.value)
       calls.createdata(message)
 
 
     } else if (message.topic == 'postprocess-sessionmgmt') {
+      message = JSON.parse(message.value)
       calls.createdata(message)
 
     }
     else if (message.topic == 'ui-sessionhistory') {
+      message = JSON.parse(message.value)
       calls.history(message)
-
     }
 
-    sess.get('/', function (req, res) {
-      if (req.session.page_views) {
-        req.session.page_views++;
-        res.send("You visited this page " + req.session.page_views + " times");
-      } else {
-        req.session.page_views = 1;
-        res.send("Welcome to this page for the first time!");
-      }
-    });
 
   });
   sess.listen(process.env.PORT);
