6c90fcc5e8f99398abfd398e4aabcb718cd5a4e1
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-03-18 22:55:57 +0000

    Added kubernetes deployment files

--- postprocessing/pyart-master/pyart/correct/rebuild_fast_edge_finder.sh
+++
@@ -1,4 +0,0 @@
-# remove and rebuild the fast.so module
-rm  _fast_edge_finder.so
-cython _fast_edge_finder.pyx
-python setup.py build_ext -i
--- usermanagement/src/main/java/com/nsa/webapp/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nsa.webapp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface UserRepository extends JpaRepository<User,Long>{
-
-	User findByUsername(String username);
-}
