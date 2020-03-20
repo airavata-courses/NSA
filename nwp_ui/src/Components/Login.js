4ecc8a265c50b62a67316390e631c74dae91782b
JShivali <jejurkar.shivali2509@gmail.com>
2020-03-20 02:54:08 -0400

    changed port for testing in kubernetes

--- nwp_ui/src/Components/Login.js
+++ nwp_ui/src/Components/Login.js
@@ -46,7 +46,7 @@ class Login extends React.Component {
     // message = JSON.parse(message);
     // console.log(msg);
     axios
-      .post("http://api:8081/login", JSON.parse(jsonobj))
+      .post("http://149.165.170.202:32450/login", JSON.parse(jsonobj))
       .then(res => {
         console.log("res_data",res.data);
         if (res.data == "LOGIN_SUCCESS") {
