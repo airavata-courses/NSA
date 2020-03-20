0d2c5a6a12eda8319bb807bcfd7d45ab362d5609
nbafna <nikitabafna04@gmail.com>
2020-03-20 17:42:05 -0400

    Adding test

--- postprocessing/test.py
+++ postprocessing/test.py
@@ -10,13 +10,13 @@ class Tests(unittest.TestCase):
     def test_dr1(self):
         print("------------------------------Testing PostProcessing 1------------------------------")
         expected = "success"
-        result = plot_radar('KTLX20160531_000848_V06.gz')
+        result,plot = plot_radar('KTLX20160531_000848_V06.gz')
         self.assertEqual(result,expected,msg = "Plot 1 failed")
 
     def test_dr2(self):
         print("------------------------------Testing PostProcessing 2------------------------------")
         expected = "success"
-        result = plot_radar('KIND20181211_000109_V06')
+        result,plot = plot_radar('KIND20181211_000109_V06')
         self.assertEqual(result,expected,msg = "Plot 2 failed")
 
 if __name__ == '__main__':
